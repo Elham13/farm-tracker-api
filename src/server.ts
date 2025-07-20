7921f1a710094dc7fd06f4afaf617bcb15d01ad7
Elham13 <elhamuddin.mahmoodi@gmail.com>
2025-07-20 20:48:26 +0530

    disable rate limit

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ import { allRoutes } from "@/api";
 import { openAPIRouter } from "@/api-docs/openAPIRouter";
 import connectDb from "@/common/db";
 import { errorHandler, notFoundRouter } from "@/common/middleware/errorHandler";
-import rateLimiter from "@/common/middleware/rateLimiter";
+// import rateLimiter from "@/common/middleware/rateLimiter";
 import { env } from "@/common/utils/envConfig";
 
 const app: Express = express();
@@ -24,7 +24,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cors({ origin: env.CORS_ORIGIN, credentials: true }));
 app.use(helmet());
 app.use(morgan("dev"));
-app.use(rateLimiter);
+// app.use(rateLimiter);
 
 // Routes
 app.use("/api", allRoutes);
